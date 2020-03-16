52ad0605d351cd5892951c8e71b0b427bf04c4c0
francoandes12 <francoande12@gmail.com>
2020-03-15 21:40:52 -0300

    "version 2.1"

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     M.AutoInit();
+    $("div#contenedor").hide();
+    $('#mostrar').click(function(){
+        $('div#contenedor').toggle();
+    })
+    .css("cursor","pointer");
 });
